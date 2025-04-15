e210aedf708d762bb99c3f7970608df565272205
Mirazovi <mdmirazhossainovi@gmail.com>
2025-04-15 09:37:45 +0600

    first commit

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+const GalleryPage = () => {
+  return (
+    <div>
+        <Image width={500} height={500} src={'https://gratisography.com/wp-content/uploads/2025/04/gratisography-cool-car-cat-1035x780.jpg'} alt='next-image' className='mx-auto'/>
+    </div>
+  )
+}
+
+export default GalleryPage
