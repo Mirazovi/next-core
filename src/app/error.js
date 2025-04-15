90708a00df01981475e5b417171ed9fdf652e2db
Mirazovi <mdmirazhossainovi@gmail.com>
2025-04-15 10:50:53 +0600

    done

---
+++ src/app/error.js
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+const error = () => {
+  return (
+    <div className='text-red-700 text-4xl text-center'>Something went Wrong !!!1</div>
+  )
+}
+
+export default error
